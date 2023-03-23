8a55d2b7d2e02cd27c48640200ac041f7d91d248
ADEOLA DARAMOLA <daradeo4u@gmail.com>
2023-03-23 13:12:36 +0000

    implement token for backend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,6 +15,9 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# Cognito Auth Serverside
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
 # Honeycomb
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -63,6 +66,13 @@ tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
+
+
+cognito_jwt_token = CognitoJwtToken(
+    user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"),
+    user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+    region=os.getenv("AWS_DEFAULT_REGION")
+)
 
 #X-RAY-----------------------------
 # XRayMiddleware(app, xray_recorder)
@@ -150,6 +160,19 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
   data = HomeActivities.run()
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    app.logger.debug(e)
+    app.logger.debug("unauthenicated")
+    data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
